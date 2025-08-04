0d6c318cebe7569ff75c39ac87f4851a8a03b419
EdgarAnt <eadv7777@gmail.com>
2025-08-03 18:02:55 -0600

    Reduce image sizes in desktop view for blog and history sections

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -69,18 +69,18 @@ const Welcome = () => {
             
             {/* Primera sección */}
             <section id="home" className="flex items-center justify-center min-h-screen px-4">
-                <div className="flex flex-col-reverse lg:flex-row items-center lg:items-start space-y-8 lg:space-y-0 lg:space-x-20">
-                    <div className="flex flex-col mt-24">
+                <div className="flex flex-col-reverse lg:flex-row items-center lg:items-start space-y-8 lg:space-y-0 lg:space-x-12">
+                    <div className="flex flex-col mt-20 lg:mt-16">
                         <img 
                             src={IMAGES.WELCOME} 
                             alt="Welcome Text" 
-                            className="w-full lg:w-[1000px] mb-2 lg:mb-6"
+                            className="w-full lg:w-[700px] mb-2 lg:mb-6"
                         />
-                        <p className="text-lg sm:text-xl lg:text-3xl text-gray-600 font-inter w-full lg:w-[1000px] h-auto lg:h-[72px] mt-16 text-center mx-auto">
+                        <p className="text-lg sm:text-xl lg:text-2xl text-gray-600 font-inter w-full lg:w-[700px] h-auto lg:h-[72px] mt-12 text-center mx-auto">
                             "Hey hello, welcome to my feelings, here you will see all about me, myself and what I'm thinking about the world"
                         </p>
                     </div>
-                    <div className="-mt-20 relative">
+                    <div className="-mt-16 lg:-mt-10 relative">
                         <div 
                             className="absolute inset-0 z-0 cursor-pointer" 
                             onClick={() => setIsModalOpen(true)}
@@ -89,7 +89,7 @@ const Welcome = () => {
                         <img 
                             src={IMAGES.ME}
                             alt="Welcome" 
-                            className="relative z-10 w-[300px] h-[300px] lg:w-[502px] lg:h-[502px] rounded-full object-cover cursor-pointer transition-transform hover:scale-105"
+                            className="relative z-10 w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] rounded-full object-cover cursor-pointer transition-transform hover:scale-105"
                             onClick={() => setShowImageQuote(true)}
                         />
                     </div>
@@ -122,7 +122,7 @@ const Welcome = () => {
                     <div className="flex flex-col lg:flex-row items-center justify-between w-full mb-8 lg:mb-12">
                         {/* COOL_GIF primero en móvil */}
                         <div className="w-full lg:w-1/2 order-1 lg:order-2 mb-4 lg:mb-0 flex justify-center">
-                            <div className="relative w-[350px] h-[350px] md:w-[500px] md:h-[500px] lg:w-[600px] lg:h-[600px]">
+                            <div className="relative w-[350px] h-[350px] md:w-[500px] md:h-[500px] lg:w-[450px] lg:h-[450px]">
                                 <img 
                                     src={IMAGES.COOL_GIF} 
                                     alt="Blog Character" 
@@ -133,7 +133,7 @@ const Welcome = () => {
                         
                         {/* BLOG_GIF segundo en móvil */}
                         <div className="w-full lg:w-1/2 order-2 lg:order-1 flex justify-center -mt-36 md:-mt-48 lg:mt-0">
-                            <div className="relative w-[350px] h-[350px] md:w-[500px] md:h-[500px] lg:w-[600px] lg:h-[600px]">
+                            <div className="relative w-[350px] h-[350px] md:w-[500px] md:h-[500px] lg:w-[450px] lg:h-[450px]">
                                 <img 
                                     src={IMAGES.BLOG_GIF} 
                                     alt="My Blog" 
@@ -174,7 +174,7 @@ const Welcome = () => {
                         <img 
                             src="/images/Dreams.png"
                             alt="Dreams" 
-                            className="w-full max-w-[1000px] h-auto object-contain mb-6"
+                            className="w-full max-w-[600px] h-auto object-contain mb-6"
                         />
                     </picture>
                     <div>
